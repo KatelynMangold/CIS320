b9cc7af8ae8cbd4d59fef8f24f82a68d69943663
Katelyn Mangold <katelyn.mangold@my.simpson.edu>
2019-01-29 15:57:30 -0600

    Starting lab 3 with Java classes

---
+++ src/edu/simpson/cis320/NameListGit.java
@@ -0,0 +1,13 @@
+package edu.simpson.cis320;
+
+import java.io.IOException;
+
+public class NameListGit extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
