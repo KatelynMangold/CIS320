fe6787c5a9c59f46daced8391a29c1efee07daeb
DESKTOP-DR4T9QF\kmang <katelyn.mangold@my.simpson.edu>
2019-02-11 21:32:12 -0600

    Lab 4

--- web/js/name_list.js
+++ web/js/name_list.js
@@ -1,1 +1,19 @@
-console.log("Hi, this is a test.");
+function updateTable()
+{
+    var url = "api/name_list_get";
+
+    $.getJSON(url, null, function (json_result)
+    {
+        for (var i = 0; i < json_result.length; i++) {
+            $('#datatable tr:first').after('<tr>' +
+                '<td>' + json_result[i].id + '</td>' +
+                '<td>' + json_result[i].first + '</td>' +
+                '<td>' + json_result[i].last + '</td>' +
+                '<td>' + json_result[i].email + '</td>' +
+                '<td>' + json_result[i].phone + '</td>' +
+                '<td>' + json_result[i].birthday + '</td>' +
+                '</tr>');
+        }
+    })
+}
+updateTable();
